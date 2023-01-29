21a624749bfc6d1b8b3173793f4699161d32c185
Ventru67 <ventru67@gmail.com>
2023-01-29 12:51:58 +0300

    done

---
+++ Ex002/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+Console.WriteLine("Задайте длинну массива : ");
+int Length = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[Length];
+void FillArray(int[] arrayColection)
+{
+    int index = 0;
+    Console.Write($"Числа массива: ");
+    while (index < arrayColection.Length)
+    {
+        arrayColection[index] = new Random().Next(1, 10);
+
+        Console.Write($" {arrayColection[index]}");
+        index++;
+    }
+    Console.WriteLine();
+}
+void PrintArray(int[] arrayColection)
+{
+    int index = 1;
+    int sum = 0;
+    while (index < arrayColection.Length)
+    {
+        sum = sum + arrayColection[index];
+        index = index + 2;
+    }
+    Console.WriteLine($"Сумма чисел одномерного массива стоящих на нечетной позиции = {sum}");
+}
+FillArray(array);
+PrintArray(array);
